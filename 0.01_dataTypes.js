b04efa23cfdd063e652367e484c22dfdfe592546
Devon Brosch <devon@brosch.org>
2017-08-16 13:13:15 -0400

    added data types and integers

---
+++ 0.01_dataTypes.js
@@ -0,0 +1,36 @@
+// DataTypes
+
+// Strings
+
+// "What a Wonderful World";
+// 'Hello you guys';
+
+// "'it's a wonderful day'";
+
+// console.log( 'Thomas said "I think grass is green!"'.toUpperCase() );
+
+// console.log( 'Couldn\'t things get any better?'.length );
+// Challenge create a string that has your name and use the Lowercase function
+// to make sure each character is Lower case.
+
+// var yourName = "Devon John Brosch";
+// console.log(yourName.toLowerCase());
+// console.log("Jay Jackson"[1]);
+// console.log('jay'[0].toUpperCase() + 'jay'[1] + 'jay'[2]);
+
+// //Challenge uppercase the "m" in "Home" and put the tring back together using string concatenation
+
+// console.log("Home"[0] + "Home"[1] + "Home"[2].toUpperCase() + "Home"[3]);
+// console.log("Devon".toUpperCase())
+
+// console.log(typeof("Amber"));
+
+// Numbers
+
+console.log(2%9);
+
+console.log(2 + 5);
+console.log(2-5);
+
+
+// Booleans
--- 0.01_integers.js
+++ 0.01_integers.js
@@ -3,3 +3,9 @@ var birthYear = 1978;
 var gradYear = 1996;
 var numOfBros = 2;
 console.log(myAge,birthYear,gradYear,numOfBros);
+var agePlusBirth = myAge + birthYear;
+var gradTimesBros = gradYear * numOfBros;
+var remainNum = agePlusBirth % gradTimesBros;
+console.log(agePlusBirth, gradTimesBros, remainNum);
+var gummyWorm = "Gummy Worm"
+console.log("Is it true that Gummy Worm is not a number?");
