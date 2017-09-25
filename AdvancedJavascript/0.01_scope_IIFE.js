56b28eaca74c34dffb442eccb7750c1e86748cf3
Devon Brosch <devon@brosch.org>
2017-09-25 09:10:49 -0400

    added IIFE file

--- AdvancedJavascript/0.01_scope_IIFE.js
+++ AdvancedJavascript/0.01_scope_IIFE.js
@@ -1,28 +1,124 @@
-var foo = "foo";
+// var foo = "foo";
 
-(function(){
-	var foo = "foo2";
-	console.log(foo); // "foo2"
+// (function(){
+// 	var foo = "foo2";
+// 	console.log(foo); // "foo2"
 
-})(); //donkey balls
+// })(); //donkey balls
 
-console.log(foo); //"foo"
+// console.log(foo); //"foo"
 
-//IIFE - Immediately Invoked Function Expression
-//run-time says, "I have an immediate value. and I can immediately execute it on line 7."
-//always name your IIFE for stack tracing.
+// //IIFE - Immediately Invoked Function Expression
+// //run-time says, "I have an immediate value. and I can immediately execute it on line 7."
+// //always name your IIFE for stack tracing.
 
-//put all of your private functions into a named IIFE
+// //put all of your private functions into a named IIFE
 
-//Pass in a global value
+// //Pass in a global value
 
-var foo = "foo";
+// var foo = "foo";
 
-(function(bar){
-	var foo = bar;
-	console.log(foo); //"foo"
+// (function(bar){
+// 	var foo = bar;
+// 	console.log(foo); //"foo"
 
-})(foo); //you can put the function call parens inside of the encompassing parens too.
-		//
+// })(foo); //you can put the function call parens inside of the encompassing parens too.
+// 		//
 
-console.log(foo); //"foo"
+// console.log(foo); //"foo"
+
+//My Stuff
+
+notIffe(2);
+
+function notIffe(notiife){
+	let nums = [notiife];
+	for (let i = 0; i < 8; i++){
+		if (notiife > 1 && notiife !== NaN && notiife !== undefined && typeof notiife !== "string"){
+		let numsIncrement = nums[i];
+		let numsCrazy = numsIncrement * notiife;
+		nums.push(numsCrazy);
+	} else if (notiife <= 1){
+		nums = []
+		nums.push("enter a bigger number than that!");
+		break
+	}else {
+		nums = []
+		nums.push("only enter a number!");
+		break
+		}
+	}
+	console.log(nums);
+}
+
+notIffe(2);
+
+function notIffe(notiife){
+	let nums = [notiife];
+	for (let i = 0; i < 8; i++){
+		if (notiife > 1 && notiife !== NaN && notiife !== undefined && typeof notiife !== "string"){
+		let numsIncrement = nums[i];
+		let numsCrazy = numsIncrement * notiife * 2;
+		nums.push(numsCrazy);
+	} else if (notiife <= 1){
+		nums = []
+		nums.push("enter a bigger number than that!");
+		break
+	}else {
+		nums = []
+		nums.push("only enter a number!");
+		break
+		}
+	}
+	console.log(nums);
+}
+
+(notIffe)(2);
+
+(function IIFE(iife){
+	let nums = [iife];
+	for (let i = 0; i < 8; i++){
+		if (iife > 1 && iife !== NaN && iife !== undefined && typeof iife !== "string"){
+			let numsIncrement = nums[i];
+			let numsCrazy = numsIncrement * iife;
+			nums.push(numsCrazy);
+		} else if (iife <= 1){
+			nums = []
+			nums.push("enter a bigger number than that!");
+			break
+		}else {
+			nums = []
+			nums.push("only enter a number!");
+			break
+		}
+}
+	console.log(nums);
+})(2)
+
+function IIFE(){
+	console.log("this is redundant");
+}
+
+IIFE();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- AdvancedJavascript/0.01_scope_blockscope.js
+++ AdvancedJavascript/0.01_scope_blockscope.js
@@ -1,30 +1,30 @@
 //let(ES6+)
 //like var but it attaches the variable implicitely in any block in which it appears
 
-function foo(){
-	var bar = "bar";
-	for (let i=0; i<bar.length; i++){
-		console.log(bar.charAt(i));
-	}
-	console.log(i);
-};
+// function foo(){
+// 	var bar = "bar";
+// 	for (let i=0; i<bar.length; i++){
+// 		console.log(bar.charAt(i));
+// 	}
+// 	console.log(i);
+// };
 
-foo();
+// foo();
 
-//attaches "i" variable to the scope block of the for loop and not to the function foo scope.
+// //attaches "i" variable to the scope block of the for loop and not to the function foo scope.
 
-function foo(bar){
-	if (bar){
-		let baz = bar;
-		if (baz){
-			let bam = baz;
-		}
-		console.log(bam);//Error
-	}
-	console.log(baz);//Error
-};
+// function foo(bar){
+// 	if (bar){
+// 		let baz = bar;
+// 		if (baz){
+// 			let bam = baz;
+// 		}
+// 		console.log(bam);//Error
+// 	}
+// 	console.log(baz);//Error
+// };
 
-foo("bar");
+// foo("bar");
 
 
 //attaches "baz" to the if statement scope block and not the function foo scope.
@@ -40,4 +40,40 @@ foo("bar");
 //refactoring can be an issue when you attempt to block scope your code, there's an added mental exercise there
 //when you have to keep track of where you put your lets.
 //let creates implicit scope blocks so it's harder to keep track of them.
+
+//My stuff
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
